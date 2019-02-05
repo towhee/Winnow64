bd8f003d320bcb0ac812538ed23a3f429bf8a394
towhee <hillrg@hotmail.com>
2019-02-05 07:53:52 -0800

    Fix merge issue

--- Utilities/utilities.cpp
+++ Utilities/utilities.cpp
@@ -35,7 +35,6 @@ QString Utilities::enquote(QString &s)
 void Utilities::hideCursor()
 {
     QApplication::setOverrideCursor(Qt::BlankCursor);
-
     // TestAsyncFileReads branch change #1
     // TestAsyncFileReads branch change #2
 }
