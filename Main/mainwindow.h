3f43ae5334f4c84135899058ce5537aaa9c029b8
towhee <hillrg@hotmail.com>
2021-04-12 07:27:01 -0700

    Sync to PC

--- Main/mainwindow.h
+++ Main/mainwindow.h
@@ -99,9 +99,10 @@ public:
     bool isReleaseVersion = true;
     bool hideEmbellish = false;
 
-    QString version = "Version: 1.19";
-//    QString version = "Version: 1.10 released 2020-06-04";
-    QString winnowWithVersion = "Winnow 1.19";
+    QString versionNumber = "1.19";
+
+    QString version = "Version: " + versionNumber;
+    QString winnowWithVersion = "Winnow " + versionNumber;
     QString website = "Website: "
 //            "<a href=\"http://165.227.46.158/winnow/winnow.html\">"
             "<a href=\"winnow.ca\">"
