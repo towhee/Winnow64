b6410579871ff9e58b2c1c0afdb67cfe82f6b074
towhee <hillrg@hotmail.com>
2022-03-21 07:18:53 -0700

    Doc Lcms2 version change process

--- Utilities/icc.h
+++ Utilities/icc.h
@@ -1,3 +1,15 @@
+/*
+    WHEN COPY NEW C CODE FROM LCMS2 AT https://github.com/mm2/Little-CMS
+
+    1. Copy .c .h files from src and include to Lcms2 in Winnow64
+
+    2. In lcms2.h uncomment as per:
+
+        // Uncomment this to remove the "register" storage class
+        // #define CMS_NO_REGISTER_KEYWORD 1
+
+*/
+
 #ifndef ICC_H
 #define ICC_H
 
