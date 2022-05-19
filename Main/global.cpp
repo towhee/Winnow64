7bedb3dc6796a0d4e2f010d4570e7cc01762c11b
towhee <hillrg@hotmail.com>
2022-05-19 07:55:23 -0700

    Remove processEvents from global

--- Main/global.cpp
+++ Main/global.cpp
@@ -133,7 +133,7 @@ namespace G
     void wait(int ms)
     {
         QTime t = QTime::currentTime().addMSecs(ms);
-        while (QTime::currentTime() < t) qApp->processEvents(QEventLoop::AllEvents, 10);
+        while (QTime::currentTime() < t); // qApp->processEvents(QEventLoop::AllEvents, 10);
     }
 
     void track(QString functionName, QString comment, bool hideTime)
